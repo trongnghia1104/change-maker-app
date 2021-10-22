431eea9ae4390e67797192e832bfd0b7b205ec06
trungit2001 <thanhtrungit2001@gmail.com>
2021-10-22 09:06:48 +0700

    save state of list prices

--- app/src/main/java/iuh_ad/phamthanhtrung/msv_19502701/changemaker/MainActivity.kt
+++ app/src/main/java/iuh_ad/phamthanhtrung/msv_19502701/changemaker/MainActivity.kt
@@ -3,7 +3,6 @@ package iuh_ad.phamthanhtrung.msv_19502701.changemaker
 import android.icu.util.UniversalTimeScale.toBigDecimal
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
 import android.view.View
 import android.widget.TextView
 import android.widget.Toast
@@ -11,14 +10,15 @@ import kotlinx.android.synthetic.main.activity_main.*
 import java.math.RoundingMode
 
 class MainActivity : AppCompatActivity() {
-    private val MAX_PRICES: Double = 20 * 9999.0
+    private val MAX_PRICE: Double = 20 * 9999.0
     private val magicNumber: Int = 6
+    private lateinit var prices: Array<TextView>
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        var prices = arrayOf(
+        prices = arrayOf(
             qty20dollars, qty10dollars,
             qty5dollars, qty1dollar,
             qty25cents, qty10cents,
@@ -58,19 +58,17 @@ class MainActivity : AppCompatActivity() {
                 return
             }
 
+            /* current amount is too big */
+            if (newPrice.length > magicNumber) {
+                Toast.makeText(this, getString(R.string.alertBigNum), Toast.LENGTH_SHORT).show()
+                return
+            }
 
             /* change current price */
             newPrice += value.toString()
             newPrice = newPrice.toInt().toString()
 
             var parsedNumber = newPrice.toDouble() / 100.0
-
-            /* current amount is too big */
-            if (parsedNumber > MAX_PRICES) {
-                Toast.makeText(this, getString(R.string.alertBigNum), Toast.LENGTH_SHORT).show()
-                return
-            }
-
             currAmount.text = parsedNumber.toString()
 
             val listPrices = arrayOf(20.0, 10.0, 5.0, 1.0, 0.25, 0.1, 0.05, 0.01)
@@ -96,11 +94,23 @@ class MainActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         var currPrice: TextView = currAmount
+        var oldPrices: ArrayList<String> = ArrayList()
+
+        for (i in prices) {
+            oldPrices.add(i.text.toString())
+        }
+
         outState.putString("currPrice", currPrice.text.toString())
+        outState.putStringArrayList("oldPrices", oldPrices)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         currAmount.text = savedInstanceState.getString("currPrice")
+        var oldPrices = savedInstanceState.getStringArrayList("oldPrices")
+
+        for (i in prices.indices) {
+            prices[i].text = oldPrices?.get(i) ?: "0"
+        }
     }
 }
